2f7a5a2d238aa8694c3398949ced0902b4488734
Wilson Huang <hjn201@gmail.com>
2016-01-19 16:55:54 +0800

    2016/01/19

--- app/src/main/java/com/main/pm25/MyDB/DBHelper.java
+++ app/src/main/java/com/main/pm25/MyDB/DBHelper.java
@@ -1,7 +1,28 @@
 package com.main.pm25.MyDB;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by WilsonHuang on 2016/1/17.
  */
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "AirStationInfo.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
